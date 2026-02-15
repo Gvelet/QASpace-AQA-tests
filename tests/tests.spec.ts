f31e6589440fe493f79dd7f33b8e6ddfa3b2c685
Milem <milemx11@gmail.com>
2026-02-15 22:29:56 +0500

    tg final fix test

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -2,36 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('🏠 Главная загружается', async ({ page }) => {
   await page.goto('/');                    // baseURL + /
-  await expect(page).toHaveTitle(/QA Spa1ce/);
-  await expect(page.locator('h1')).toBeVisible();
-});
-
-test('🏠 Главная загружается-2', async ({ page }) => {
-  await page.goto('/');                    // baseURL + /
   await expect(page).toHaveTitle(/QA Space/);
-  await expect(page.locator('h1')).toBeVisible();
-});
-
-test('🏠 Главная загружается-3', async ({ page }) => {
-  await page.goto('/');                    // baseURL + /
-  await expect(page).toHaveTitle(/QA Space/);
-  await expect(page.locator('h1')).toBeVisible();
-});
-
-test('🏠 Главная загружается-4', async ({ page }) => {
-  await page.goto('/');                    // baseURL + /
-  await expect(page).toHaveTitle(/QA Spa1ce/);
-  await expect(page.locator('h1')).toBeVisible();
-});
-
-test('🏠 Главная загружается-5', async ({ page }) => {
-  await page.goto('/');                    // baseURL + /
-  await expect(page).toHaveTitle(/QA Spa1ce/);
-  await expect(page.locator('h1')).toBeVisible();
-});
-
-test('🏠 Главная загружается-11', async ({ page }) => {
-  await page.goto('/');                    // baseURL + /
-  await expect(page).toHaveTitle(/QA Sp1ace/);
   await expect(page.locator('h1')).toBeVisible();
 });
