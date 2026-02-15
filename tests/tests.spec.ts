e842c8d15cbbf6272c6185358df8a0b3fd9a587d
Milem <milemx11@gmail.com>
2026-02-15 20:05:39 +0500

    fail test

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('🏠 Главная загружается', async ({ page }) => {
   await page.goto('/');                    // baseURL + /
-  await expect(page).toHaveTitle(/QA Space/);
+  await expect(page).toHaveTitle(/QA Spa1ce/);
   await expect(page.locator('h1')).toBeVisible();
 });
