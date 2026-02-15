ad9cec0396532c449ffbb6b4e00708c0c2176e79
Milem <milemx11@gmail.com>
2026-02-15 21:12:01 +0500

    tg push

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -5,3 +5,21 @@ test('🏠 Главная загружается', async ({ page }) => {
   await expect(page).toHaveTitle(/QA Spa1ce/);
   await expect(page.locator('h1')).toBeVisible();
 });
+
+test('🏠 Главная загружается-2', async ({ page }) => {
+  await page.goto('/');                    // baseURL + /
+  await expect(page).toHaveTitle(/QA Space/);
+  await expect(page.locator('h1')).toBeVisible();
+});
+
+test('🏠 Главная загружается-3', async ({ page }) => {
+  await page.goto('/');                    // baseURL + /
+  await expect(page).toHaveTitle(/QA Space/);
+  await expect(page.locator('h1')).toBeVisible();
+});
+
+test('🏠 Главная загружается-4', async ({ page }) => {
+  await page.goto('/');                    // baseURL + /
+  await expect(page).toHaveTitle(/QA Spa1ce/);
+  await expect(page.locator('h1')).toBeVisible();
+});
